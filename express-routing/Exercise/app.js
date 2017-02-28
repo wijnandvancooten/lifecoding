78db9082981c2d6f40c386160b18fba4dbfebec8
wijnandvancooten <wijnandvancooten@hotmail.com>
2017-02-28 15:46:28 +0100

    set the first routes

--- express-routing/Exercise/app.js
+++ express-routing/Exercise/app.js
@@ -3,12 +3,18 @@ const express = require('express')
 //create our app from the library
 const app = express()
 
+//require the text.js file and set it to a cons named text
+const text = require(__dirname + '/routes/text')
 
-//import router from directory /routes/user
-//const user = require(__dirname + '/routes/user')
-
+//uses the poblic file to display the html file that is inside.
 app.use(express.static('public'))
 
+//uses the path /patato and de module text.
+app.use('/potato', text)
+
+app.get('/', (req, res) => {
+  console.log('someone is visiting us on the dead cat page ;)')
+})
 
 //make the app listen to localhost:300
 app.listen (3000, f => {
--- express-routing/Exercise/public/custom.js
+++ express-routing/Exercise/public/custom.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
 	console.log('dom had leaded')
 	$('p').hide()
-	$('h1').click(function(){
+	$('button').click(function(){
 		console.log('h1 has been clicked')
-		$(this).hide()
-		$('p').slideDown(5000).hide(5000).slideDown(5000).rotate(+5)
+		$('.showHide').slideDown(5000).hide(5000).slideDown(5000).rotate(+5)
 
 	})
 
 })
-
